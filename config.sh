aadc893adc7e0e9d26faf2e363830a9b3da62e59
Sanjay Goraniya <sgoraniya22@gmail.com>
2021-11-27 15:20:44 +0530

    Update : git config script

---
+++ config.sh
@@ -0,0 +1,11 @@
+git config --local --list
+
+
+git config --local user.name "Sanjay Goraniya"
+git config --local user.email sgoraniya22@gmail.com
+
+echo ''
+echo "****************"
+echo ''
+
+git config --local --list
